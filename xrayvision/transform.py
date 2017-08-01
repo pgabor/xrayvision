a04f8beea8b4a3e78bd0ed241b64e09d441812e6
Gábor Péterffy <peterffy95@gmail.com>
2017-08-01 14:22:29 +0200

    Irregular sampling

--- xrayvision/transform.py
+++ xrayvision/transform.py
@@ -12,10 +12,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pylab
 
+class visibility:
+    def __init__(self):
+        self.visibility = 0.0j
+        self.k = (0, 0)
 
 def inner_fourier(data: np.array, k: np.array, sign: float):
     """
-    This function returns the inner part of the 2D DFT for outer_fourier
+    This function returns the inner part of the 2D DFT for dft
 
     Parameters
     ----------
@@ -35,7 +39,7 @@ def inner_fourier(data: np.array, k: np.array, sign: float):
 
     See Also
     --------
-        outer_fourier()
+        dft()
 
     Notes
     -----
@@ -54,7 +58,46 @@ def inner_fourier(data: np.array, k: np.array, sign: float):
     return result
 
 
-def outer_fourier(data, sign):
+def inner_fourier_for_bag(data: np.array, k: np.array, dim: tuple):
+    """
+    This function returns the inner part of the 2D DFT for dft
+    from visibility bag
+
+    Parameters
+    ----------
+    data : np.array
+        List of the known visibilities
+
+    k : np.array
+        The result shuld be calculated for this
+
+    dim : tuple
+        The dimensions of the desired image
+
+    Returns
+    -------
+        The corresponding value for k as a result of Fourier transformation
+
+    See Also
+    --------
+        dft()
+
+    Notes
+    -----
+
+    Reference
+    ----------
+    """
+    result = 0.0j
+    for i in data:
+        n = np.array([complex(i.k[0]), complex(i.k[1])])
+        q = np.divide(k, np.array([complex(dim[0]), complex(dim[1])]))
+        inside = np.dot(np.multiply(n, 2.0j), np.transpose(q))
+        result += np.exp(inside) * i.visibility
+    return result
+
+
+def dft(data, sign):
     """
     This function returns with the 2D DFT for the given data with the
     appropiate conversation direction
@@ -90,7 +133,71 @@ def outer_fourier(data, sign):
     return result
 
 
-def example():
+def dft_from_bag(vis, dim):
+    """
+    This function returns with the 2D DFT for a given visibility bag
+
+    Parameters
+    ----------
+    vis : np.array
+        List of the visibilities
+
+    dim : tuple
+        Contains the desired size of the final image
+
+    Returns
+    -------
+        Complex numpy array with the calculated Fourier-transformed data
+
+    See Also
+    --------
+        dft()
+
+    Notes
+    -----
+
+    Reference
+    ----------
+    """
+    result = np.zeros(dim, dtype=np.complex)
+    for i in range(dim[0]):
+        for j in range(dim[1]):
+            result[i, j] = inner_fourier_for_bag(vis, np.array([i, j]), dim)
+    return result
+
+def create_visibility_bag(positions: np.array, data: np.array):
+    """
+    Creates a visibility bag from the given positions
+
+    Parameters
+    ----------
+    positions : np.array
+        Calculate the visibilities there
+    data : np.array
+        The intensity data
+
+    Returns
+    -------
+        np.array with visibility objects for the given positions
+
+    See Also
+    --------
+
+    Notes
+    -----
+
+    Reference
+    ----------
+    """
+    result = []
+    for i in positions:
+        res = visibility()
+        res.visibility = inner_fourier(data, np.array([i]), -1.0)
+        res.k = i
+        result.append(res)
+    return result
+
+def example1():
     """
     This function creates an "image" and tries to convert it twice,
     it shows a plot for each intermediate step
@@ -104,7 +211,7 @@ def example():
 
     See Also
     --------
-        outer_fourier()
+        dft()
 
     Notes
     -----
@@ -115,13 +222,45 @@ def example():
     im = np.ones((15, 15))
     im[10, 5] = 100
     im[10, 10] = 150
-    vis = outer_fourier(im, -1.0)
+    vis = dft(im, -1.0)
     plt.imshow(im)
     pylab.show()
     plt.imshow(np.real(vis))
     pylab.show()
     plt.imshow(np.imag(vis))
     pylab.show()
-    im_b = outer_fourier(vis, 1.0)
+    im_b = dft(vis, 1.0)
+    plt.imshow(np.real(im_b))
+    pylab.show()
+
+
+def example2():
+    """
+    This function creates an "image" and tries to convert it twice,
+    it shows a plot for each intermediate step - visibility bag
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+        Nothing
+
+    See Also
+    --------
+        dft_from_bag()
+
+    Notes
+    -----
+
+    Reference
+    ----------
+    """
+    im = np.ones((15, 15))
+    im[10, 5] = 100
+    im[10, 10] = 150
+    vis = create_visibility_bag(np.array([[1, 1], [1, 13], [13, 1], [13, 13]]), im)
+    plt.imshow(im)
+    im_b = dft_from_bag(vis, (15, 15))
     plt.imshow(np.real(im_b))
     pylab.show()
